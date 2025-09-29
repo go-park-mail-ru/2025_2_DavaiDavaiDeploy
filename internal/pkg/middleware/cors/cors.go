364277e5f5177b1af7c5c65a2b1fdcbcdce38b5f
Elizaveta-Makeeva <makeevaea18@gmail.com>
2025-09-29 13:54:54 +0300

    cors

--- internal/pkg/middleware/cors/cors.go
+++ internal/pkg/middleware/cors/cors.go
@@ -7,8 +7,8 @@ import (
 func CorsMiddleware(next http.Handler) http.Handler {
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Methods", "POST,GET")
-		w.Header().Set("Access-Control-Allow-Headers", "Authorization,Content-Type,X-Csrf-Token")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH")
+		w.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Expose-Headers", "Authorization,X-Csrf-Token")
 		w.Header().Set("Access-Control-Allow-Origin", "*")
