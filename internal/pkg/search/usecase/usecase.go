bdcb125ca331f8278b43673b4d12db3e73422d9d
Elizaveta-Makeeva <makeevaea2020@yandex.ru>
2025-11-20 13:26:12 +0300

    fix: empty response

--- internal/pkg/search/usecase/usecase.go
+++ internal/pkg/search/usecase/usecase.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"kinopoisk/internal/models"
 	"kinopoisk/internal/pkg/search"
+	"kinopoisk/internal/pkg/utils/log"
+	"log/slog"
 )
 
 type SearchUsecase struct {
@@ -17,18 +19,32 @@ func NewSearchUsecase(repo search.SearchRepo) *SearchUsecase {
 }
 
 func (uc *SearchUsecase) GetFilmsFromSearch(ctx context.Context, searchString string, pager models.Pager) ([]models.MainPageFilm, error) {
+	logger := log.GetLoggerFromContext(ctx).With(slog.String("func", log.GetFuncName()))
+
 	mainPageFilms, err := uc.searchRepo.GetFilmsFromSearch(ctx, searchString, pager.Count, pager.Offset)
 	if err != nil {
 		return []models.MainPageFilm{}, err
+	}
+
+	if len(mainPageFilms) == 0 {
+		logger.Info("no films")
+		return []models.MainPageFilm{}, nil
 	}
 
 	return mainPageFilms, nil
 }
 
 func (uc *SearchUsecase) GetActorsFromSearch(ctx context.Context, searchString string, pager models.Pager) ([]models.MainPageActor, error) {
+	logger := log.GetLoggerFromContext(ctx).With(slog.String("func", log.GetFuncName()))
+
 	mainPageActors, err := uc.searchRepo.GetActorsFromSearch(ctx, searchString, pager.Count, pager.Offset)
 	if err != nil {
 		return []models.MainPageActor{}, err
+	}
+
+	if len(mainPageActors) == 0 {
+		logger.Info("no actors")
+		return []models.MainPageActor{}, nil
 	}
 
 	return mainPageActors, nil
