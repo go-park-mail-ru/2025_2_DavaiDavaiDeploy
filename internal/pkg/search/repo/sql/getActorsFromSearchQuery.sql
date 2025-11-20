111c5c6cfe44b829c0ead9cfe899d3af3a2921ac
DanKazh <117713084+DanKazh@users.noreply.github.com>
2025-11-20 12:43:09 +0300

    Update getActorsFromSearchQuery.sql

--- internal/pkg/search/repo/sql/getActorsFromSearchQuery.sql
+++ internal/pkg/search/repo/sql/getActorsFromSearchQuery.sql
@@ -1,10 +1,9 @@
 SELECT 
     a.id,
     a.russian_name,
-    a.photo,
-    ts_rank(a.tsvector_column, plainto_tsquery('ru', $1)) as relevance
+    a.photo
 FROM actor a
 WHERE a.tsvector_column @@ plainto_tsquery('ru', $1)
     AND ts_rank(a.tsvector_column, plainto_tsquery('ru', $1)) >= 0.3
-ORDER BY relevance DESC
+ORDER BY ts_rank(a.tsvector_column, plainto_tsquery('ru', $1)) DESC
 LIMIT $2 OFFSET $3;
