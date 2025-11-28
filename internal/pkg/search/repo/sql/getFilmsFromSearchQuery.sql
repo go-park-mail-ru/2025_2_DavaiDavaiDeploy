d893031d3926696ca452ef900c2f92b0e2e9d330
Elizaveta-Makeeva <makeevaea2020@yandex.ru>
2025-11-29 01:51:55 +0300

    fix: search

--- internal/pkg/search/repo/sql/getFilmsFromSearchQuery.sql
+++ internal/pkg/search/repo/sql/getFilmsFromSearchQuery.sql
@@ -11,17 +11,22 @@ SELECT
 FROM film f
 JOIN genre g ON f.genre_id = g.id
 WHERE 
-    (f.tsvector_column @@ plainto_tsquery('ru', $1) AND ts_rank(f.tsvector_column, plainto_tsquery('ru', $1)) >= 0.3)
+    (f.tsvector_column @@ plainto_tsquery('ru', lower($1)) AND ts_rank(f.tsvector_column, plainto_tsquery('ru', lower($1))) >= 0.3)
     OR
-    (f.tsvector_column @@ plainto_tsquery('en', $1) AND ts_rank(f.tsvector_column, plainto_tsquery('en', $1)) >= 0.3)
+    (f.tsvector_column @@ plainto_tsquery('en', lower($1)) AND ts_rank(f.tsvector_column, plainto_tsquery('en', lower($1))) >= 0.3)
     OR
-    (
-      similarity(f.title, $1) >= 0.3 OR similarity(f.original_title, $1) >= 0.3 
-      OR similarity(f.description, $1) >= 0.3 OR similarity(f.short_description, $1) >= 0.3
+	(
+	    SELECT bool_or(
+            token ILIKE '%' || search_word || '%' OR 
+            (length(search_word) > 3 AND similarity(token, search_word) >= 0.2)
+        )
+        FROM unnest(string_to_array(lower($1), ' ')) as search_word
+        CROSS JOIN unnest(tsvector_to_array(f.tsvector_column)) as token
+        WHERE search_word != ''
     )
 ORDER BY 
     GREATEST(
-        ts_rank(f.tsvector_column, plainto_tsquery('ru', $1)),
-        ts_rank(f.tsvector_column, plainto_tsquery('en', $1))
+        ts_rank(f.tsvector_column, plainto_tsquery('ru', lower($1))),
+        ts_rank(f.tsvector_column, plainto_tsquery('en', lower($1)))
     ) DESC
 LIMIT $2 OFFSET $3;
