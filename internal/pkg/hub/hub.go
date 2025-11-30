e6a2d5d61ed71864056d260674db068073144181
Elizaveta-Makeeva <makeevaea2020@yandex.ru>
2025-11-30 22:00:00 +0300

    fix: nginx for ws

--- internal/pkg/hub/hub.go
+++ internal/pkg/hub/hub.go
@@ -6,6 +6,7 @@ import (
 	"sync"
 	"time"
 
+	"kinopoisk/internal/models"
 	"kinopoisk/internal/pkg/films/repo"
 	"kinopoisk/internal/pkg/utils/log"
 
@@ -45,26 +46,25 @@ func (h *Hub) Run(ctx context.Context) {
 		select {
 		case <-t.C:
 			news, ok := h.Repo.GetUpdates(ctx, h.currentOffset)
-			if !ok {
+			if !ok || len(news) == 0 {
 				h.currentOffset = time.Now()
 				continue
 			}
-			logger.Info("sending news to all clients", "count:", len(news))
+
+			logger.Info("sending news to all clients", "count", len(news))
+
 			h.connect.Range(func(key, value interface{}) bool {
 				conn := key.(*websocket.Conn)
 				if conn == nil {
 					h.connect.Delete(key)
 					return true
 				}
-				if err := conn.WriteJSON(news); err != nil {
-					h.connect.Delete(key)
-					_ = conn.Close()
-					logger.Error("failed to write news: ", err)
-					return true
-				}
+
+				go h.sendNewsToClient(conn, news, logger)
 
 				return true
 			})
+
 			h.currentOffset = time.Now()
 
 		case <-ctx.Done():
@@ -76,5 +76,18 @@ func (h *Hub) Run(ctx context.Context) {
 			})
 			return
 		}
+	}
+}
+
+func (h *Hub) sendNewsToClient(conn *websocket.Conn, news []models.News, logger *slog.Logger) {
+	for _, newsItem := range news {
+		if err := conn.WriteJSON(newsItem); err != nil {
+			h.connect.Delete(conn)
+			conn.Close()
+			logger.Error("failed to write news, connection closed", "error", err)
+			return
+		}
+
+		time.Sleep(5 * time.Millisecond)
 	}
 }
