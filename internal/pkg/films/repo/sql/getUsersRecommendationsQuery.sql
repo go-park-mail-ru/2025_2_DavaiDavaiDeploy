2ee1a1e0eea5d0fe6a8694aba9d5fe2b37a868c5
DanKazh <kazhenets06@yandex.ru>
2025-12-04 22:46:43 +0300

    fix: users ds algorithm

--- internal/pkg/films/repo/sql/getUsersRecommendationsQuery.sql
+++ internal/pkg/films/repo/sql/getUsersRecommendationsQuery.sql
@@ -3,7 +3,7 @@ SELECT
     f.country_id, f.duration, f.cluster_id,
     ROUND(COALESCE(AVG(ff_all.rating), 0), 1) as avg_rating,
     COUNT(ff_all.rating) as rating_count,
-    COALESCE(ff_user.rating, 0) as user_rating,
+    COALESCE(ff_user.rating, 0.0) as user_rating,
     f.cover
 FROM film f 
 LEFT JOIN film_feedback ff_all ON f.id = ff_all.film_id 
