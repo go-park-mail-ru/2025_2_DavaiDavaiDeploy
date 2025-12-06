01b0dad4e0f70e127c0c902e629f1d3c73ad50aa
Elizaveta-Makeeva <makeevaea2020@yandex.ru>
2025-12-06 16:06:56 +0300

    fix: grantUserDB

--- build/sql/grantUserDB.sql
+++ build/sql/grantUserDB.sql
@@ -1,22 +1,25 @@
 GRANT CONNECT ON DATABASE kinopoisk TO ddfilms_user;
 
 GRANT SELECT, INSERT, UPDATE, DELETE ON 
+    film_feedback,
+    fav_films
+TO ddfilms_user;
+
+GRANT SELECT ON 
     actor,
     actor_in_film,
     film,
-    film_feedback,
-    fav_films,
     news_table,
     compilation,
     film_in_compilation,
-    user_table
-TO ddfilms_user;
-
-
-GRANT SELECT ON 
     country,
     genre
 TO ddfilms_user;
+
+
+GRANT INSERT ON user_table TO ddfilms_user;
+GRANT UPDATE (avatar, password_hash, has_2fa, secret_code, updated_at) 
+ON user_table TO ddfilms_user;
 
 
 GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO ddfilms_user;
