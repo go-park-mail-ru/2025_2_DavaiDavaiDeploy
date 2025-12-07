2af8bec10cc61b3293d347d554f7a5352960e4c5
Elizaveta-Makeeva <makeevaea2020@yandex.ru>
2025-12-07 19:03:34 +0300

    fix: 16 utc

--- build/sql/createDB.sql
+++ build/sql/createDB.sql
@@ -406,7 +406,7 @@ BEGIN
             'Сегодня премьера фильма "' || COALESCE(NEW.title, NEW.original_title) || '"! 🎬 ',
             COALESCE(NEW.short_description, 'Скоро будет больше информации.'),
             NEW.id,
-            (NEW.release_date::timestamp + INTERVAL '19 hours')
+            (NEW.release_date::timestamp + INTERVAL '16 hours 25 minutes')
         );
     END IF;
     RETURN NEW;
