7a55e2af936d9ad31d9fee5d51529bc228559733
DanKazh <kazhenets06@yandex.ru>
2025-12-11 23:55:04 +0300

    choire

--- build/sql/createDB.sql
+++ build/sql/createDB.sql
@@ -55,7 +55,7 @@ CREATE TABLE IF NOT EXISTS film (
     budget bigint,
     worldwide_fees bigint,
     trailer_url text,
-	film_url text,
+	film_url text DEFAULT '',
     year integer NOT NULL,
     country_id uuid NOT NULL,
     cluster_id integer,
