e1c3566934a92f943e2efc1b7197d47cdce4fa51
Elizaveta-Makeeva <makeevaea2020@yandex.ru>
2025-12-21 19:00:48 +0300

    fix: login length

--- perf_test/createUsers.lua
+++ perf_test/createUsers.lua
@@ -1,29 +1,18 @@
 wrk.method = "POST"
-local counter = 0
+local counter = 100000 
 local total_users = 100000
-
-local unique_prefix = tostring(math.random(1000000000, 9999999999))
-math.randomseed(os.time() * 1000)
+local password = "Test123!"
 
 request = function()
     counter = counter + 1
-    if counter > total_users then
+    if counter > 200000 then 
         return nil
     end
     
-    local login = string.format("%s_%d_%d", 
-        unique_prefix, 
-        counter, 
-        math.random(1000, 9999))
+    local login = "user" .. tostring(counter)
     
-
-    login = login:sub(1, 20)
     
-    if #login < 6 then
-        login = login .. string.rep("x", 6 - #login)
-    end
-    
-    local body = string.format('{"login": "%s", "password": "password123"}', login)
+    local body = string.format('{"login": "%s", "password": "%s"}', login, password)
     
     local headers = {
         ["Content-Type"] = "application/json",
@@ -34,5 +23,5 @@ request = function()
 end
 
 done = function(summary, latency, requests)
-    print(string.format("\nCreated %d users", counter - 1))
+    print(string.format("\nCreated %d users", counter - 100000))
 end
